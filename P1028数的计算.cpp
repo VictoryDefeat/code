653dd283e78a18c6e7ee0a7010bc5baf6653a9e0
VictoryDefeat <44114929+VictoryDefeat@users.noreply.github.com>
2019-08-27 18:44:38 +0800

    Delete P1028数的计算.cpp

--- P1028数的计算.cpp
+++
@@ -1,9 +0,0 @@
-#include<cstdio>
-int n,f[1001];
-int main()
-{
-    scanf ("%d",&n);
-    for (int i=1;i<=n;i++)f[i]=1;
-    for(int i=1;i<=n;i++)for(int j=1;j<=i/2;j++)f[i]+=f[j];
-    printf ("%d",f[n]);
-}
